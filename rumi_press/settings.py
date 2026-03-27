4bc3562dc88d3734c44bbb9e6d01fc5562a1b497
hoanggddo <hoangg.ddo@gmail.com>
2026-03-27 19:59:41 -0400

    Update settings.py

--- rumi_press/settings.py
+++ rumi_press/settings.py
@@ -19,11 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7i*))^1a^srj5_%a)=*8+2zie-_i-c1&3mebpzq7tsvsly=_^n'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
 
 ALLOWED_HOSTS = ['rumi-press-22f5.onrender.com']
 
